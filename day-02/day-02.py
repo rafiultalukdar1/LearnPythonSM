ca693302c7289dae65ac25ed59be3a769b441e25
rafi0012 <rafiultalukdar007@gmail.com>
2026-09-24 22:39:06 +0600

    Explore Variable & Input/Output Function

--- day-02/day-02.py
+++ day-02/day-02.py
@@ -1,1 +1,10 @@
-print('Hello World', 'welcome to python', sep='! ', end='.')
+print('Hello World', 'welcome to python', sep='! ', end='.\n')
+
+first_name = 'Rafiul'
+last_name = 'Talukdar'
+age = 25
+
+def func01():
+    roll = 66
+    print('Name: ' + first_name + ' '+ last_name, 'Age: ' + str(age), 'Roll: ' + str(roll), sep=', ', end='.')
+func01()
