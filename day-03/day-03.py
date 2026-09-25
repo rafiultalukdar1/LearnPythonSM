56549d478ee470a29800de64e6f611a567bb722a
rafi0012 <rafiultalukdar007@gmail.com>
2026-09-25 14:00:34 +0600

    Explore Data Type

--- day-03/day-03.py
+++ day-03/day-03.py
@@ -1,2 +1,32 @@
+# -----Data Type-----
+
+# integer data type
+x = 7
+
+# sring data type
+a = "Abdullah"
+
+# float data type
+y = 3.20
+
+# boolean data type
+z = True
+
+# complex data type
+ab = 2 + 5j
+
+# print(type(x))
+# print(type(a))
+# print(type(y))
+# print(type(z))
+# print(type(ab))
+
+
 # Input Function
 
+# manik = input()
+# print(type(manik))
+
+first_number = int(input('Enter your first number: '))
+second_number = int(input('Enter your second number: '))
+print('Total number:', first_number + second_number)
