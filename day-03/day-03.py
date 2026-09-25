164920373312a5d26ca0854ec041e1c811e48c87
rafi0012 <rafiultalukdar007@gmail.com>
2026-09-25 14:52:44 +0600

    Explore Datatype

--- day-03/day-03.py
+++ day-03/day-03.py
@@ -15,18 +15,33 @@ z = True
 # complex data type
 ab = 2 + 5j
 
-# print(type(x))
-# print(type(a))
-# print(type(y))
-# print(type(z))
-# print(type(ab))
+print(type(x))
+print(type(a))
+print(type(y))
+print(type(z))
+print(type(ab))
 
 
 # Input Function
 
-# manik = input()
-# print(type(manik))
+manik = input()
+print(type(manik))
 
 first_number = int(input('Enter your first number: '))
 second_number = int(input('Enter your second number: '))
 print('Total number:', first_number + second_number)
+
+# Batter hoy float use korle
+
+third_number = float(input('Enter your third number: '))
+fourth_number = float(input('Enter your fourth number: '))
+print('Total number:', third_number + fourth_number)
+
+
+data = 'Day-3.0 - 60 Days of Python'
+print(type(data))
+print(len(data))
+import sys
+print(sys.getsizeof(data))
+sub_data = 'Python'
+print(data.count(sub_data))
